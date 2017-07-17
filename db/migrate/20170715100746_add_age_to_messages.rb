5f74d5f2b100f2d4b67bc1c42a6cf85163558599
takahiro suzuki <taiyoyodai@gmail.com>
2017-07-17 11:07:09 +0000

    Modify template

--- db/migrate/20170715100746_add_age_to_messages.rb
+++ db/migrate/20170715100746_add_age_to_messages.rb
@@ -2,8 +2,4 @@ class AddAgeToMessages < ActiveRecord::Migration[5.0]
   def change
     add_column :messages, :age, :integer
   end
-<<<<<<< HEAD
 end
-=======
-end
->>>>>>> 668d2967bf0ae6cb38d297e1f2bed344c41e5ec6
